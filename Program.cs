389d2ed9639ad574e847ce62d6d66a220de7e655
acin <mukhlishinmarsyad@students.amikom.ac.id>
2021-03-16 20:49:01 +0700

    Tugas Membuat Class Taxi

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TugasTaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Membuat objek Taxi
+            Taxi taxi = new Taxi();
+
+            // Pengesetan nilai Properties
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            // Pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TugasTaxi
+{
+    class Taxi
+    {
+        // Properties
+        public string DriverName { get; set; }
+        public Boolean OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        // Method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name: {0}", DriverName);
+
+            if (OnDuty) Console.WriteLine("On Duty: Yes");
+            else Console.WriteLine("On Duty: No");
+            Console.WriteLine("Number Of Passenger: {0}\n", NumPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("Jono sedang menjemput penumpang");
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("Jono selesai mengantar penumpang");
+        }
+
+
+    }
+}
